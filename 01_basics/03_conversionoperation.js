9066c8b62e727d78a4f074f9876c61be2792be5b
Ayush-Singh-Tanwar <itzayushsingh4321@gmail.com>
2025-01-23 07:25:41 +0000

    conversion datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,43 @@
+//FOR NUMBERS/*************************************************/
+
+/*let score = 30
+// let score = "30abc"
+// let score = null
+// let score = undefined
+// let score = true
+// let score = "Ayush"
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0*/
+
+
+//FOR BOOLEAN/*************************************************/
+
+/*// let isLoggedIn = 1
+let isLoggedIn = "Ayush"
+// let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+//"ayush" => true*/
+
+
+//FOR STRINGS/*************************************************/
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
